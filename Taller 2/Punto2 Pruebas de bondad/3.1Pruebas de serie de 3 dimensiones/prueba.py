04e94e2b9915783de9ee563d61ac1681ece5b229
EduardoSP <eduardo.saavedra@correounivalle.edu.co>
2015-10-14 16:48:06 -0500

    combinaciones de numeros
    Se emcuentra la prueba para las combinaciones de los numeros

---
+++ Taller 2/Punto2 Pruebas de bondad/3.1Pruebas de serie de 3 dimensiones/prueba.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+ #http://www.lawebdelprogramador.com/foros/Python/1379063-Combinatoria.html
+for i in range(1,4):
+    for j in range(1,4):
+        for k in range(1,4):
+            print "%s%s%s" % (i,j,k)
